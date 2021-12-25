e6d22fc280ccea0f58ab1923b880b72384e0847c
Tommy Bi <bisi@thayer.com>
2021-12-25 15:46:25 -0500

    Week 1 of R Language Programming

---
+++ Consule Input & Evaluation.R
@@ -0,0 +1,20 @@
+
+#  "<-" is the assignment operator
+x <- 5
+
+# Both are ways to print 
+print(x) # 5, explicit printing
+x # 5, auto-printing
+
+# prints [1] 5, indicating x is a vector and 
+#               the first element is 5
+
+
+# String 
+msg <- "Hello"
+msg # "Hello", auto-printing
+
+
+# Integer Sequences 
+y <- 1 : 20
+y  # 1 - 20
