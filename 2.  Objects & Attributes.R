6c679b4f792b6b774ad0501fb4159b705a0b3ef1
Tommy Bi <bisi@thayer.com>
2021-12-27 09:14:42 -0500

    Update

---
+++ 2.  Objects & Attributes.R
@@ -0,0 +1,18 @@
+# Five basic classes of objects 
+
+# character 
+# numeric (real numbers)
+# integer
+# complex
+# logical (True, False)
+
+# The MOST BASIC object is a vector
+  # Can contain objs ONLY of the SAME class
+
+  # vector() to create an empty vector
+  
+  # EXCEPTION: 
+    # List: A vector containing objs from diff classes
+
+v <- vector()
+v
